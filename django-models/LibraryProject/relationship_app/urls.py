853d14064d26d53b3197064d14622f03504f8be8
fetsevi <etsevifrancois@gmail.com>
2025-09-07 13:13:04 +0000

    Add of django views and template html

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.book_list, name="book_list"),  # FBV
+    path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # CBV
+]
+
