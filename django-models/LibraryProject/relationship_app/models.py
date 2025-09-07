e86536a94142894c3adb31e7cc08490664dd2cde
fetsevi <etsevifrancois@gmail.com>
2025-09-07 17:09:00 +0000

    Add of django models, view and url user role based access

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -33,3 +33,32 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+# Extend the user model with user profile
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+# Automatically create or update UserProfile when a User is created/updated
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.profile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -48,3 +48,35 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return render(request, "relationship_app/logout.html")
+
+#Role based view
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+
+# Helper functions to check roles
+def is_admin(user):
+    return hasattr(user, "profile") and user.profile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "profile") and user.profile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "profile") and user.profile.role == "Member"
+
+
+# Views restricted by role
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
