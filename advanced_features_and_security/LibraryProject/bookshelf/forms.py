2c7b6c08268a822a136bd73164bb6aaf0b5081ee
fetsevi <etsevifrancois@gmail.com>
2025-09-14 17:54:34 +0000

    Add of Implemented security best update

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import Book
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=True)
+    
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author", "published_date"]
