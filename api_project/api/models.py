0e065916eac9bb6d03ff36bcfa9b4d0f38c3b783
fetsevi <etsevifrancois@gmail.com>
2025-09-18 08:05:54 +0000

    Add of api_project with model book

---
+++ api_project/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Book model
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
