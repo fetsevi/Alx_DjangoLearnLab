74468adede4a7dc78cc420213139b72cda620973
fetsevi <etsevifrancois@gmail.com>
2025-09-25 11:52:44 +0000

    Add of filters, search, and order ing in BookViewList

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, filters
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
