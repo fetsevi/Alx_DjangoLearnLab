dab58e379fff7f55ab33e3e0d1d31c5c5f950a9f
fetsevi <etsevifrancois@gmail.com>
2025-09-25 09:37:59 +0000

    Add of generics and mixin views in advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from .models import Book
 from .serializers import BookSerializer
