68ca375a8a3bd3e709c73971fa50f4a8b817270d
fetsevi <etsevifrancois@gmail.com>
2025-10-03 06:50:11 +0000

    Add of CRUD Operations to blog App

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
-    path("post/<int:pk>/delete", views.PostDeleteView.as_view(), name='post_delete'),
-                   
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),               
                    # Login, Register Url
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
